4d71b6931785954382810bf3f559246d42ff5299
otknoy <otknoy@gmail.com>
2015-06-25 17:37:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
     name='dmm-search',
     version='0.0.1',
-    description="DMM search API",
-    py_mdules=['dmm'],
+    description="DMM search API for Python",
+    py_modules=['dmm'],
     package_dir={'': 'src'},
+    keywords='dmm, search',
     author='otknoy',
     author_email='otknoy@gmail.com',
     url='https://github.com/otknoy/dmm_search_api',

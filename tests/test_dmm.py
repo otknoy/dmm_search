bd25ee1a27ed99c70fb860c37a92380dcf77f65d
otknoy <otknoy@gmail.com>
2016-10-09 19:23:05 +0900

    Use nosetests

--- tests/test_dmm.py
+++ tests/test_dmm.py
@@ -1,6 +1,13 @@
-import unittest
+from unittest import TestCase
+from nose.tools import ok_, eq_
 import dmm
 
-class TestDMM(unittest.TestCase):
+class TestDMM(TestCase):
+    def setUp(self):
+        print('before test')
+
+    def tearDown(self):
+        print('after test')
+
     def test_hoge(self):
-        self.assertTrue(dmm.DMM)
+        ok_(dmm.DMM)
